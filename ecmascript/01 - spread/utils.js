64a216cc49d40904854bed2527db2c1e9b392fb8
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2018-05-23 15:23:39 -0300

    Remove Babel Finger Exercise in this branch

--- ecmascript/01 - spread/utils.js
+++
@@ -1,9 +0,0 @@
-// This have a linter problem, it says that we should prefer
-// export default
-// Would you like to fix this issue ?
-
-export function isArray(aPossibleArray) {
-  return aPossibleArray.constructor === Array;
-}
-
-// export default isArray;
--- ecmascript/02 - promises/index.js
+++
@@ -1,9 +0,0 @@
-// Hint: use setInterval, create a new Promise and measure time with Date.now()
-
-export function delay() {
-
-}
-
-export function asyncDelay() {
-
-}
