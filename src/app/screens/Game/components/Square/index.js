0a04b7130a3424c7d667b886739da4ac3c5c5397
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2018-05-24 18:18:57 -0300

    An Interactive Component

--- src/app/screens/Game/components/Square/index.js
+++ src/app/screens/Game/components/Square/index.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import './styles.css';
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: null };
+  }
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
-Square.propTypes = {
-  value: PropTypes.number
-};
+// Square.propTypes = {
+//   value: PropTypes.number
+// };
 
 export default Square;
