571f54254833a9da317e8b0ed9d103aebf3fae15
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2018-05-24 16:22:36 -0300

    Change to relative path

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './scss/index.scss';
 import registerServiceWorker from './registerServiceWorker';
-
-import Game from '~screens/Game'; // eslint-disable-line import/first
+import Game from './app/screens/Game';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
