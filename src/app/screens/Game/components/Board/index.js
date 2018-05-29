83a91f37991ba0e477546c0711b5c02e0c204aaf
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2018-05-29 12:23:28 -0300

    Lifting State Up

--- src/app/screens/Game/components/Board/index.js
+++ src/app/screens/Game/components/Board/index.js
@@ -5,9 +5,20 @@ import Square from '../Square/index.js';
 import './styles.css';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { squares: Array(9).fill(null) };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares });
+  }
+
   // eslint-disable-next-line
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
--- src/app/screens/Game/components/Square/index.js
+++ src/app/screens/Game/components/Square/index.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import './styles.css';
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: null };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
-// Square.propTypes = {
-//   value: PropTypes.number
-// };
+Square.propTypes = {
+  value: PropTypes.number,
+  onClick: PropTypes.func
+};
 
 export default Square;
