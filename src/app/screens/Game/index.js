e5a795fcc62ec323b0d4d032d652fff0a4fd5ef1
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2018-05-29 14:48:10 -0300

    Showing the Moves

--- src/app/screens/Game/index.js
+++ src/app/screens/Game/index.js
@@ -50,6 +50,15 @@ class Game extends Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = `Winner: ${winner}`;
@@ -64,7 +73,7 @@ class Game extends Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
