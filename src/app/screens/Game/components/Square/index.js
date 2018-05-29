999f3f16b0e412d8da8ca0553e7748275bd470a9
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2018-05-29 12:46:26 -0300

    Functional Components

--- src/app/screens/Game/components/Square/index.js
+++ src/app/screens/Game/components/Square/index.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './styles.css';
 
-class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 Square.propTypes = {
