86245977ae1e70810057f73842335872bf5d704a
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2018-06-18 11:44:19 -0300

    PR changes

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 import { create } from 'apisauce';
 
-const baseURL = ' http://192.168.122.1:3005';
+const baseURL = process.env.API_BASE_URL;
 
 const api = create({
   baseURL,
@@ -11,11 +11,6 @@ const api = create({
 api.addMonitor(Reactotron.apisauce);
 // eslint-disable-next-line
 export const apiSetup = dispatch => {
-  // eslint-disable-line no-unused-vars, prettier/prettier
-  if (baseURL === 'http://wolox.com') {
-    console.warn('API baseURL has not been properly initialized');
-  }
-
   api.addMonitor(response => {
     if (response.status === 401) {
       // dispatch(actions.sessionExpired());
